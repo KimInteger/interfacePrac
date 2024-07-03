59ba4ba37a72d7c09a07291561727e043adb0828
KimInteger <kjs20151240@gmail.com>
2024-07-03 20:07:29 +0900

    feat_김정수 : 함수 interface

--- src/interfaceTwo.ts
+++ src/interfaceTwo.ts
@@ -1,1 +1,10 @@
-console.log('two');
+interface StringFormatter {
+  (input : string, upper : boolean) : string;
+}
+
+const formatString : StringFormatter = (input, upper) => {
+  return upper ? input.toUpperCase() : input.toLowerCase();
+}
+
+console.log(formatString("Hellow Interface!" , true));
+console.log(formatString("Hellow Interface!" , false));
