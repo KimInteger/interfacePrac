41dde9514552143e0af40ff886d52bb2dc15e071
KimInteger <kjs20151240@gmail.com>
2024-07-03 20:55:10 +0900

    feat_김정수 : 선택적 속성의 사용

--- src/interfaceFive.ts
+++ src/interfaceFive.ts
@@ -1,1 +1,33 @@
-console.log('hellow! five!');
+interface Product {
+  id : number;
+  name : string;
+  price : number;
+  description? : string; // 선택적 속성
+}
+
+const product : Product = {
+  id : 1,
+  name : "보름달",
+  price : 500000
+}
+
+const printProductDetails = (product : Product) : void => {
+  console.log(`ID : ${product.id}`);
+  console.log(`Name : ${product.name}`);
+  console.log(`Price : ${product.price}`);
+  if(product.description) {
+    console.log(`Description : ${product.description}`);
+  } else {
+    console.log(`Description : 설명이 읎오!`);
+  }
+}
+
+const sell : Product = {
+  id : 2,
+  name : "초승달",
+  price : 600000,
+  description : "보름달 보다 비싼이유는 왜일까?"
+}
+
+printProductDetails(product);
+printProductDetails(sell);
