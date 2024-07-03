dae013d2dcdc929305e878611484b4de910cb2e9
KimInteger <kjs20151240@gmail.com>
2024-07-03 20:50:33 +0900

    feat_김정수 : extend확장사용

--- src/interfaceFour.ts
+++ src/interfaceFour.ts
@@ -1,1 +1,23 @@
-console.log('hellow! Four!');
+interface Person {
+  name : string;
+  age : number;
+}
+
+interface Employee extends Person {
+  employeeId : number;
+  department : string;
+}
+
+const employee : Employee = {
+  name : "Kiminteger",
+  age : 29,
+  employeeId : 20151240,
+  department : "furniture installation"
+};
+
+
+const getEmployeeInfo = (emp: Employee) : string => {
+  return `Name : ${emp.name}, Age : ${emp.age}, Employee Id : ${emp.employeeId}, Department : ${emp.department}`;
+};
+
+console.log(getEmployeeInfo(employee));
