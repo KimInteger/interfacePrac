d63bb42a1019b345f58cd5f92b92208b811ead15
KimInteger <kjs20151240@gmail.com>
2024-07-03 20:01:09 +0900

    feat_김정수 : interface를 사용한 객체생성

--- src/interfaceOne.ts
+++ src/interfaceOne.ts
@@ -1,1 +1,13 @@
-console.log('firstInterface');
+interface User {
+  id : number;
+  name : string;
+  email : string;
+}
+
+const getUserInfo = (user:User) : string => {
+  return `ID : ${user.id}, Name : ${user.name}, Email : ${user.email}`;
+}
+
+const user : User = {id:1, name: "KimInteger", email : "talton@naver.com"};
+
+console.log(getUserInfo(user));
