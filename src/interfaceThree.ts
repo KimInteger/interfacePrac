72d5d2a14071205547c6a8dd0f9675da15d54441
KimInteger <kjs20151240@gmail.com>
2024-07-03 20:43:41 +0900

    feat_김정수 : 클래스 인터페이스 구현

--- src/interfaceThree.ts
+++ src/interfaceThree.ts
@@ -1,1 +1,31 @@
-console.log('Hellow, Three!');
+interface Animal {
+  name : string;
+  sound() : void;
+}
+
+class Dog implements Animal {
+  name : string;
+  constructor(name: string) {
+    this.name = name;
+  }
+  sound(): void {
+    console.log(`${this.name}는 왈!`);
+  }
+}
+
+class Cat implements Animal {
+  name : string;
+  constructor(name : string){
+    this.name = name;
+  }
+  sound(): void {
+    console.log(`${this.name}은 야옹`);
+  }
+}
+
+const dog = new Dog('강아지');
+
+const cat = new Cat('고양이');
+
+dog.sound();
+cat.sound();
